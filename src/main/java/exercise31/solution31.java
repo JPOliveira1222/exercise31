2adb67946373bb67eda60c68ef136774eb129f77
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-03 18:48:02 -0400

    pseudocode for 31

---
+++ src/main/java/exercise31/solution31.java
@@ -0,0 +1,33 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 31 Solution
+ *  Copyright 2021 Joao Pedro Oliveira
+ */
+package exercise31;
+
+import java.util.Scanner;
+
+public class solution31{
+
+    /*
+    set 2 variables for restingPulse and age
+    print "enter your resting Pulse"
+    restingPulse = user input
+
+    print "Enter your age: "
+    age = user input
+
+    print "Resting Pulse: "+restingPulse+ " Age: " +age"
+
+    set a variable for the intensity
+
+    print out the table
+
+    using a while loop the equation at a 5% interval
+
+    print out the results
+
+     */
+
+
+
+}
